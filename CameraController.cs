5a859b69e1e4613e10e477c5023885a3869cdd4d
Snehi <111358189+SnehiCodes@users.noreply.github.com>
2023-01-30 23:03:43 +0530

    Add files via upload

--- CameraController.cs
+++ CameraController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
     public Transform player;
 
     private void Update()
