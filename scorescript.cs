fabd3eef9292a50f2b5b004fdd9c2ee58f3ab917
Snehi <111358189+SnehiCodes@users.noreply.github.com>
2023-03-08 23:30:01 +0530

    Add files via upload

---
+++ scorescript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class scorescript : MonoBehaviour
+{
+
+    public static int scoreValue ;
+    public TextMeshProUGUI score;
+   // Text score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = ""+ scoreValue ;
+    }
+}
