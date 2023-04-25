c02d79a4686e4e1b228699faa278ea86ee9c5f0d
Snehi <111358189+SnehiCodes@users.noreply.github.com>
2023-04-25 13:03:41 +0530

    Add files via upload

--- fall.cs
+++ fall.cs
@@ -5,14 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class fall : MonoBehaviour
 {
-    [SerializeField]private string sceneToLoad;
+    //[SerializeField]private string sceneToLoad;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
-            PermanentUI.perm.Reset();
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            // PermanentUI.perm.Reset();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            HealthManager.health--;
+            PlayerController.heart -=1;
         }
     }
 }
