6edce2e859f6d8bd32247a9ba12100267963f303
Snehi <111358189+SnehiCodes@users.noreply.github.com>
2023-04-25 13:03:13 +0530

    Add files via upload

--- Enemy.cs
+++ Enemy.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     protected Animator anim;
-    [SerializeField] protected Rigidbody2D rb;
+    protected Rigidbody2D rb;
+    protected AudioSource death;
 
     protected virtual void Start()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        death = GetComponent<AudioSource>();
     
     }
     public void JumpedOn()
     {
         anim.SetTrigger("Death");
+        death.Play();
+        enemyScript.scoreValue +=1;
         rb.velocity = Vector2.zero;
     }
     private void Death()
     {
+        
         Destroy(this.gameObject);
+        
     }
 }
